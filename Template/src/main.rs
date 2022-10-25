3f82c1f460633e32554bed08a63d4ecf0121c55d
Alex Colello <alexlcolello@gmail.com>
2022-10-25 14:48:07 -0400

    cleanup

--- Template/src/main.rs
+++ Template/src/main.rs
@@ -2,13 +2,11 @@ const PART: u8 = 1;
 
 fn part1() {
     let result = 0;
-
     println!("Answer for part 1: {}", result);
 }
 
 fn part2() {
     let result = 0;
-
     println!("Answer for part 2: {}", result);
 }
 
